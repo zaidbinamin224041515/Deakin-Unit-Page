65671cd490e9299838a35bc315b3deb8b9b3f9b3
zaidbinamin224041515 <zaidbinamin04@gmail.com>
2024-09-30 20:17:16 +1000

    Update pipeline-script.sh

--- pipeline-script.sh
+++ pipeline-script.sh
@@ -1,1 +1,12 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building the project...'
+                bat 'mvn clean package'
+            }
+        }
+    }
+}
 
